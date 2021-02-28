2288f68044135834a76b91b38f54160fa5ab48ef
Jrock30 <jrg8989@knou.ac.kr>
2021-02-28 14:25:10 +0900

    Day 2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,46 @@ import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
+
+        /**
+         * 영속성 컨텍스트
+         *
+         * 엔티티를 영구 저장하는 환경
+         * EntityManager.persist(entity)
+         * 디비에 저장한다는 것이 아니라 영속성 컨텍스를 통해서 엔티티를 영속화 한다는 뜻(영속성 컨텍스트에 저장)
+         * 영속성 컨텍스트는 논리적인 개념이다.(엔티티 매니저를 통해 영속성 컨텍스트에 접근, 엔티티매니저 1 : 1 영속성 컨텍스트)
+         * 어플리케이션과 디비 사이에 중간 계층이 있다고 이해
+         *
+         * 이점
+         * 1차캐시
+         *   - find -> 영속성 컨텍스트 -> DB    --> 한 트랜잭션 안에서만 작동하니 큰 이점은 없으나, 가끔 복잡한 로직에서만 도움 됨
+         * 동일성(identity)보장
+         *   - 영속엔티티의 같은 데이터를 == 비교하면 동일성 보장(자바 컬렉션 조회에서 == 비교 하듯이)
+         *   - 1차 캐시로 반복 가능한 읽기(REPEATABLE READ) 등급의 트랜잭션 격리 수준을 데이터베이스가 아닌 애플리케이션 차원에서 제공
+         * 트랜잭션을 지원하는 쓰기 지연(transactional write-behind)
+         *   - em.persist(member) 할 적에 INSERT 쿼리를 보내는 것이 아닌 tx.commit(); 할 때 FLUSH가 되면서 INSERT 쿼리를 보낸다.
+         *   - 영속 컨텍스트(entityManager) 안에 쓰기 지연 SQL 저장소에 쿼리를 먼저 쌓아둠. (1차 캐시에도 데이터 저장)
+         * 변경감지(Dirty Checking), 지연로딩(Laze Loading)
+         *   - 데이터를 find 한 다음에 객체의 값을 수정해주기만 해도 수정된 데이터가 커밋이 된다.
+         *   - flush() -> 엔티티와 스냅샷 비교(1차캐시 비교, 1차캐시 내부에 최초시점의 스냅샷을 떠 놓음) -> 쓰기지연 SQL저장소(SQL 생성) -> flush -> commit
+         *   - 변경을 체크해서 update 쿼리를 날리거나, insert를 날리거나 하는 분기를 둘 필요가 없음.
+         *   - on duplicate key update (X)
+         *
+         * 플러쉬
+         *   - em.flush() -> 직접호출 ( 쿼리를 미리 보고 싶을 때 활용 )
+         *   - 트랜잭션 커밋  -> 자동호출
+         *   - JPQL 호출   -> 자동호출
+         *   - 영속성 컨텍스트를 비우지 않음
+         *   - 영속성 컨텍스트의 변경내용 데이터베이스에 동기화
+         *   - 트랜잭션이라는 작업 단위가 중요 -> 커밋 직전에만 동기화 하면 됨.
+         *
+         * 준 영속성
+         *   - em.detach(entity)  -> 특정 엔티티만 준영속 상태로 전환
+         *   - em.clear()         -> 영속성 컨텍스트를 완전히 초기화(테스트 케이스 작성할 때 도움됨)
+         *   - em.close()         -> 영속성 컨텍스트를 중료
+         *
+         */
+
         /**
          *
          * 모든 JPA 에서는 트랜잭션 단위가 엄청 중요하다. 모든작업은 트랜잭션 안에서 작업을 해야한다.
@@ -31,6 +71,7 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
+
         /**
          * 엔티티 객체 멤버를 만들어준다.
          */
@@ -38,6 +79,7 @@ public class JpaMain {
         try {
             /**
              * INSERT
+             * 객체생성(비영속상태)
              */
 //            Member member = new Member();
 //            member.setId(2L);
@@ -63,6 +105,8 @@ public class JpaMain {
              * 데이터베이스에 종속되지 않는 JPQL 대표적으로 아래와 같이 디비에 따라 바뀐다.
              * 페이지네이션(오라클이면 rownum, MYSQL 이면 limit offset 디비에 따라 바뀜)
              * 한마디로 객체지향 SQL
+             * JPQL을 날릴 때는 무조건 flush()가 발생함. 그래서 객체를 persist 하고 커밋을 하지 않아도 flush가 발동해서 조회가 됨
+             *
              */
 //            List<Member> result = em.createQuery("SELECT m FROM Member AS m", Member.class)
 //                    .getResultList();
@@ -89,11 +133,20 @@ public class JpaMain {
 //            findMember.setName("HelloJPA");
 
             /**
-             *  이러면 저장이 된다.
+             *  이러면 저장이 된다.(이 때 디비에 저장되는 것은 아님, 쿼리가 나가는 타이밍은 아님)
+             *  객체를 저장한 상태(영속 상태)
+             *  영속성 컨텍스트 안에 들어가서 관리
              */
 //            em.persist(member);
+//            em.detach(member); // 회원 엔티티를 영속성 컨텍스트에서 분리, 준영속 상태
+//            em.remove(member); // 객체를 삭제한 상태(삭제)
 
-            // 트랜잭션 커밋
+//            em.flush() -> 쿼리 날림(수동)
+
+            /**
+             * 트랜잭션 커밋
+             * 이 때 디비에 쿼리가 날라감
+             */
             tx.commit();
         } catch (Exception e) {
             // Exception 발생시 rollback
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -11,9 +11,22 @@ public class Member {
 
     @Id // PK
     private Long id;
-
 //    @Column(name = "username") 이렇게하면 username 으로 컬럼명이 명시되어 쿼리 나감
     private String name;
+
+    //
+
+    /**
+     * JPA 내부적으로 리플렉션 같은 것을 사용하기 때문에 그냥 생성자를 쓰면 에러 발생
+     * 기본 생성자가 하나 있어야함. (public으로 꼭 할 필요는 없음
+     */
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
